156535f4de220f43d70742759de539ca8f517ad7
SontuCoder <subhadipmaity791@gmail.com>
2024-07-31 13:34:18 +0530

    add

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const apikey="5329c77fca38e8923f27bd1eabb6fda1"
 const apiurl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=kolkata";
 
+setInterval(()=>{
+    let d = new Date();
+    document.querySelector("#time").innerHTML = d.toLocaleTimeString();
+},1000)
+
 async function checkWeather(){
     const responce =  await fetch(apiurl + `&appid=${apikey}`);
     let data = await responce.json();
